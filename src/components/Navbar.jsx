d98950be4ad846778662e177c253b342b5d548b3
prince bakenga <princebakenga09032017@gmail.com>
2022-02-18 17:09:43 +0100

    header init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import { Search } from "@material-ui/icons";
+
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Language = styled.div`
+  font-size: 14;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div``
 
 const Navbar = () => {
-  return <div>My Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>The Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
